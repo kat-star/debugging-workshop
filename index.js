01a148f702c9de48a0aec3648545088ccc37d6ab
kat-star <katherinewkwong@gmail.com>
2019-11-03 18:00:41 -0800

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
+  const userInput = document.getElementById('name-input')
+  
   let joke;
 
-  function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+  function fetchJoke() {
+    fetch('https://icanhazdadjoke.com/'
+    , {
       headers: {
         "Accept": "application/json"
       }
     })
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(jokeData => {
+      joke = jokeData.joke; //moved into the fetch otherwise it will load before fetch() is done and get set to undefined
+      const newJokeLi = document.createElement('li') //moved newJokeLi here to create a new one each time
+      newJokeLi.innerHTML = `
+      <span class="username">${userInput.value} says:</span> ${joke}
+      `;
+      jokeList.appendChild(newJokeLi);
+    })
   }
 
   form.addEventListener('submit', (event) => {
-
-    if(username === "") return;
+    event.preventDefault(); //needed to add this in since a submit wants to post
+    if(userInput.value === "") return;
     fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
   })
-})
+});
